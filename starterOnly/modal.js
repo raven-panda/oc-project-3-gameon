de9f6d804f580a3a044cf1e3beef1b6d7ae2be9b
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-07 16:34:02 +0200

    dev-closemodal/add: added close button in modal, added event for modal closing - fixes OpenClassrooms-Student-Center#1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,28 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/** DOM Elements **/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelector("#close-modal-btn")
 const formData = document.querySelectorAll(".formData");
 
+/** Events listeners **/
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+modalCloseBtn.addEventListener("click", closeModal);
 
-// launch modal form
+/**
+ * Launches modal form
+ */
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+/**
+ * Closes modal form
+ */
+function closeModal() {
+  modalbg.style.display = "none";
+}
